a869d1d28d67a82c38939ac9fe28313784d31ad9
marianilchychyn-ctrl <marianilchychyn@gmail.com>
2026-09-10 13:28:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'student-hub-v15';
+const CACHE_NAME = 'student-hub-v16';
 const ASSETS = [
   './',
   './index.html',
@@ -9,7 +9,6 @@ const ASSETS = [
   './design-refresh.css',
   './app.js',
   './manifest.json',
-  './logo_kn.jpg',
   './icon-192.png',
   './icon-512.png'
 ];
